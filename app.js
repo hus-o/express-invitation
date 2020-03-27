2b29f619929578d3b4d787f4ab8ba6156e5b492d
huseyin <thehusko@gmail.com>
2020-03-27 15:57:42 +0000

    able to view form and submit results

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
-
+const path = require("path")
 app.use(express.urlencoded({extended:false}));
 app.use(express.static("public"));
 app.use(express.static("views"));
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/views/invitation.html'));;
+  });
+
+app.post("/card", (req,res) =>{
+    res.send(req.body)
+    
+})
+
+        
 app.listen(PORT, () =>{
     console.log(`Listening here: http://localhost:${PORT}`);
 })
